308eba31131a7f11928e6c1853d35eaa125ab45e
psainathreddy <sainathreddy4118@gmail.com>
2025-01-09 10:34:27 +0530

    javascript

---
+++ practice.js
@@ -0,0 +1,20 @@
+var container= document.getElementById("container");
+var image= document.createElement("img");
+document.container.style.border="1px solid black";
+
+function balayya(){
+    image.src="./balayya.jpeg"
+    container.appendChild(image);
+}
+function allu(){
+    image.src="./allu.jpeg"
+    container.appendChild(image);
+}
+function prabhas(){
+    image.src="./prabhas.jpeg"
+    container.appendChild(image);
+}
+function yash(){
+    image.src="./yash.jpeg"
+    container.appendChild(image);
+}
---
+++ task-1.js
@@ -0,0 +1,6 @@
+ function callhim(){
+ var container= document.getElementById("container");
+ container.style.background="yellow";
+ document.body.style.background="red";
+var text=document.getElementById("text")
+ }
